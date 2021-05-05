57a96ec9e94702e7a06ee4a9c7124c2d4a22ee83
imam <imamabdulazis028@gmail.com>
2021-05-05 15:41:13 +0700

    update env

--- pages/api/v1/users/index.js
+++ pages/api/v1/users/index.js
@@ -1,40 +1,47 @@
 const { PrismaClient } = require('@prisma/client');
 import authenticateToken from '../../helper/autenticate_jwt'
 
-const handler = async (req, res) => {
+export default async (req, res) => {
     const prisma = new PrismaClient()
 
     switch (req.method) {
         case "GET":
 
-            //validate jwt token
-            const isAuth = authenticateToken(req, res)
-            if (!isAuth) return res.status(401).json({
-                status: 401,
-                message: "Token expired"
-            })
+            try {
+                //validate jwt token
+                const isAuth = authenticateToken(req, res)
+                if (!isAuth) return res.status(401).json({
+                    status: 401,
+                    message: "Token expired"
+                })
 
-            const users = await prisma.users.findMany({
-                select: {
-                    id: true,
-                    name: true,
-                    email: true,
-                    username: true,
-                    previlage: true,
-                    telp_number: true,
-                    image_url: true,
-                    created_at: true,
-                    deleted_at: true,
-                    updated_at: true,
-                }
-            })
+                const users = await prisma.users.findMany({
+                    select: {
+                        id: true,
+                        name: true,
+                        email: true,
+                        username: true,
+                        previlage: true,
+                        telp_number: true,
+                        image_url: true,
+                        created_at: true,
+                        deleted_at: true,
+                        updated_at: true,
+                    }
+                })
 
-            if (!users) return res.status(404).json({ status: 404, message: "User tidak ditemukan" })
-            return res.status(200).json({ status: 200, message: "Ok", data: users })
+                if (!users) return res.status(404).json({ status: 404, message: "User tidak ditemukan" })
+                return res.status(200).json({ status: 200, message: "Ok", data: users })
+            } catch (e) {
+                console.log(e);
+                return res.status(500).json({
+                    status: 500,
+                    message: e
+                })
+            }
         default:
             return res.status(405).json({ status: 405, message: 'Request method tidak di izinkan' })
     }
 }
 
-export default handler
 
