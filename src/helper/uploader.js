68bd3d488fcdcade04564b1b53c4a50d71371f5a
Imam <imam.azis@pgi-data.com>
2021-08-23 02:31:35 +0700

    update file

--- src/helper/uploader.js
+++ src/helper/uploader.js
@@ -4,56 +4,49 @@
  */
 
 // const { format } = require('util');
-const { Storage } = require('@google-cloud/storage');
-const config = require('@utils/firebase');
-const { v4: uuid } = require('uuid');
-
-
-const storage = new Storage({
-    projectId: config.projectId,
-    keyFilename: "./src/helper/service_account.json"
-});
-
-const bucket = storage.bucket(config.storageBucket);
+const { Storage } = require("@google-cloud/storage");
+const config = require("@utils/firebase");
+const { v4: uuid } = require("uuid");
+import fileJson from "./service_account.json";
 
 const uploadImageToStorage = (file) => {
-    return new Promise((resolve, reject) => {
-        let generatedToken = uuid();
-        if (!file) {
-            reject('No image file');
-        }
 
-        let newFileName = `${file.originalname}`;
+  const storage = new Storage({
+    projectId: config.projectId,
+    keyFilename: "./src/helper/service_account.json",
+  });
 
-        let fileUpload = bucket.file(newFileName);
+  const bucket = storage.bucket(config.storageBucket);
 
-        const blobStream = fileUpload.createWriteStream({
-            metadata: {
-                contentType: file.type
-            },
-        });
+  return new Promise((resolve, reject) => {
+    let generatedToken = uuid();
+    if (!file) {
+      reject("No image file");
+    }
 
-        blobStream.on('error', (error) => {
-            console.log(error);
-            reject('Something is wrong! Unable to upload at the moment.');
-        });
+    let newFileName = `${file.originalname}`;
 
-        blobStream.on('finish', (response) => {
-            // const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
-            const url = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${fileUpload.name}?alt=media&token=${generatedToken}`;
-            resolve(url);
-        });
+    let fileUpload = bucket.file(newFileName);
 
-        blobStream.end(file.buffer);
+    const blobStream = fileUpload.createWriteStream({
+      metadata: {
+        contentType: file.type,
+      },
     });
-}
 
-module.exports = { uploadImageToStorage }
+    blobStream.on("error", (error) => {
+      console.log(error);
+      reject("Something is wrong! Unable to upload at the moment.");
+    });
 
+    blobStream.on("finish", (response) => {
+      // const url = format(`https://storage.googleapis.com/${bucket.name}/${fileUpload.name}`);
+      const url = `https://firebasestorage.googleapis.com/v0/b/${config.storageBucket}/o/${fileUpload.name}?alt=media&token=${generatedToken}`;
+      resolve(url);
+    });
 
+    blobStream.end(file.buffer);
+  });
+};
 
-
-
-
-
-
+module.exports = { uploadImageToStorage };
